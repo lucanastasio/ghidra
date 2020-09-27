f2c572cd76ba4b3d6012b6abbfdfeab4b18a80a5
chrisps <chss95cs@gmail.com>
2020-09-27 14:23:51 -0700

    2312: Implement Lzcnt zero test rule in the decompiler
    Update ruleaction.hh
    Add implementation of lzcnt zero test rule
    Add lzcnt zero test rule to the analysis action group

--- Ghidra/Features/Decompiler/src/decompile/cpp/coreaction.cc
+++ Ghidra/Features/Decompiler/src/decompile/cpp/coreaction.cc
@@ -5414,6 +5414,7 @@ void ActionDatabase::universalAction(Architecture *conf)
 	actprop->addRule( new RuleOrMultiBool("analysis") );
 	actprop->addRule( new RuleXorSwap("analysis") );
 	actprop->addRule( new RuleLzcountShiftBool("analysis") );
+	actprop->addRule( new RuleLzcntZeroTest("analysis") );
 	actprop->addRule( new RuleSubvarAnd("subvar") );
 	actprop->addRule( new RuleSubvarSubpiece("subvar") );
 	actprop->addRule( new RuleSplitFlow("subvar") );
